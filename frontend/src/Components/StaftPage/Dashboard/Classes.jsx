d97fb8505e86f7388c1c65e89cf910caa729c8ab
Harsha643 <battuharsha6@gmail.com>
2025-06-30 13:13:36 +0530

    changed classes.css

--- frontend/src/Components/StaftPage/Dashboard/Classes.jsx
+++ frontend/src/Components/StaftPage/Dashboard/Classes.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import '../DashboardStyles/Classes.css';
+import "../DashboardStyles/classes.css";
 import Header from './Header'; // Adjust path if needed
 
 const Classes = () => {
