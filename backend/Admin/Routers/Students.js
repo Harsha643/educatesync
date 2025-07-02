eb6df7a73d6204049315a5755f0a67b1ace27739
Harsha643 <battuharsha6@gmail.com>
2025-07-02 11:52:35 +0530

    update uploads

--- backend/Admin/Routers/Students.js
+++ backend/Admin/Routers/Students.js
@@ -2,12 +2,13 @@ const express = require("express");
 const StudentsRouter = express.Router();
 const StudentController = require("../Controllers/Students");
 const multer = require("multer");
+const path=require("path")
 
 // Set up multer storage
 const storage = multer.diskStorage({
 
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, path.join(__dirname, '../uploads'));
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
